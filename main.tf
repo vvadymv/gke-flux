f4d5c337374c6aaf448914171929d1b511e6d9fe
Vadym Vedmedenko <vadim.vedmedenko@gmail.com>
2024-08-29 00:27:40 +0300

    Implement kind cluster

--- main.tf
+++ main.tf
@@ -4,22 +4,24 @@ terraform {
       source  = "hashicorp/google"
       version = "5.42.0"
     }
+    kind = {
+      source = "tehcyx/kind"
+      version = "0.6.0"
+    }
   }
   backend "gcs" {
     bucket = "m7-devops-tfstate"
-    prefix = "tf-init"
+    prefix = "tf-flux"
   }
 }
 
-provider "google" {
-  project = var.GOOGLE_PROJECT
-  region  = var.GOOGLE_REGION
-}
+provider "kind" {}
 
-module "gke_cluster" {
-  source         = "github.com/vvadymv/tf-google-gke-cluster"
-  GOOGLE_REGION  = var.GOOGLE_REGION
-  GKE_NAME = var.GKE_NAME
-  GKE_NUM_NODES  = var.GKE_NUM_NODES
-  GKE_MACHINE_TYPE = var.GKE_MACHINE_TYPE
+#locals {
+#    k8s_config_path = pathexpand("/tmp")
+#}
+
+resource "kind_cluster" "default" {
+    name = var.GKE_NAME
+#    kubeconfig_path = local.k8s_config_path
 }
