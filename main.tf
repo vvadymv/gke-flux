169bded77b0c360d07521eb4b5f85e1b45116524
Vadym Vedmedenko <vadim.vedmedenko@gmail.com>
2024-08-31 16:45:06 +0300

    Fix kubernetes credentials for flux

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/google"
       version = "5.42.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.2"
+    }
     kind = {
       source = "tehcyx/kind"
       version = "0.6.0"
@@ -23,17 +27,38 @@ terraform {
   }
   backend "gcs" {
     bucket = "m7-devops-tfstate"
-    prefix = "tf-flux-gke"
+    prefix = "tf-flux-gke2"
   }
 }
 
 # GKE cluster
+data "google_client_config" "default" {}
+
 module "gke_cluster" {
   source         = "github.com/vvadymv/tf-google-gke-cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GKE_NAME = var.GKE_NAME
   GKE_NUM_NODES  = var.GKE_NUM_NODES
   GKE_MACHINE_TYPE = var.GKE_MACHINE_TYPE
+}
+
+## Authentication gathering
+module "gke_auth" {
+  depends_on = [module.gke_cluster.this]
+  
+  source               = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  project_id           = var.GOOGLE_PROJECT
+  cluster_name         = var.GKE_NAME
+  location             = var.GOOGLE_REGION
+}
+
+## Generate kubeconfig
+resource "local_file" "kubeconfig" {
+  depends_on = [module.gke_auth.this]
+
+  content  = module.gke_auth.kubeconfig_raw
+  filename = "/home/vadymv/.kube/config_gke-flux-kbot"
+  file_permission = "0600"
 }
 
 # Github repo
@@ -60,7 +85,7 @@ resource "github_repository_deploy_key" "this" {
 
 # Setup cluster with flux
 resource "flux_bootstrap_git" "this" {
-  depends_on = [github_repository_deploy_key.this]
+  depends_on = [github_repository_deploy_key.this, local_file.kubeconfig]
 
   embedded_manifests = true
   path               = "clusters/my-cluster"
